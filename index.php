8dce1d2e67aae8822c2d49ef45b6e766009a6013
andikaopp <andikaopp@gmail.com>
2017-02-21 12:21:05 +0700

    versi 1.0.0

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pawon Studio Developer | Home :: PSD</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html: charset=utf-8">
+	<meta name="keywords" content="Pawon Studio">
+	<script type="application/x-javascript">
+		addEventListener("load",
+			function(){
+				setTimeout(hideURLbar,0);
+			},
+			false);
+		function hideURLbar(){
+			window.scrollTo(0,1);
+		}
+	</script>
+	<!-- Custom CSS Link -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+	<!-- // Custom CSS Link -->
+	<!-- Fonts -->	
+	<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' 	type='text/css'>
+	<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Sigmar+One' 			type='text/css'>
+	<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Raleway:400,500,600'		type='text/css'>
+	<!-- //Fonts -->
+	<!-- JavaScript -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script src="js/responsiveslides.min.js"></script>
+	<script>
+		$(function(){
+			$("#slider1").responsiveSlides({
+				auto: true,
+				nav: true,
+				speed: 1000,
+				namespace: "callbacks",
+				pager: true,
+			});
+		});
+	</script>
+	<!-- //JavaScript -->
+	<!-- animation effects -->
+	<link href="css/animate.min.css" rel="stylesheet"> 
+	<script src="js/wow.min.js"></script>
+	<script>
+		new WOW().init();
+	</script>
+	<!-- //animation effects -->
+</head>
+<body>
+	<!-- Header -->
+	<div class="header">
+		<!-- navbar -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header wow fadeInDown animated" data-wow-delay=".5s" style="visibility: visible;animation-delay:0.5s; animation-name: fadeInDown; ">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php">Pawon.Std</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav wow fadeInUp animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+						<li>
+							<a href="about.html" class="hover-effect scroll">
+								<span>
+									<span>ABOUT</span>
+									<span>ABOUT</span>
+									<span></span>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="services.html" class="hover-effect scroll">
+								<span>
+									<span>SERVICES</span>
+									<span>SERVICES</span>
+									<span></span>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="gallery.html" class="hover-effect scroll">
+								<span>
+									<span>GALLERY</span>
+									<span>GALLERY</span>
+									<span></span>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="codes.html" class="hover-effect scroll">
+								<span>
+									<span>CODES</span>
+									<span>CODES</span>
+									<span></span>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="contact.html" class="hover-effect scroll">
+								<span>
+									<span>CONTACT</span>
+									<span>CONTACT</span>
+									<span></span>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<!-- //navbar -->
+	</div>
+	<!-- //Header -->
+	<!-- Slider -->
+	<div class="slider">
+		<ul class="rslides" id="slider1">
+			<li>
+				<img src="images/slide-1.jpg" alt="Wanderer">
+				
+				<div class="caption">
+					<h3>Trial and Error Your Code</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/slide-2.jpg" alt="Wanderer">
+				<div class="caption">
+					<h3>Dont Bored with your error</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/slide-1.jpg" alt="Wanderer">
+				<div class="caption">
+					<h3>You'll be happy when your error solved</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/slide-2.jpg" alt="Wanderer">
+			
+				<div class="caption">
+					<h3>Stay Tune in your logic</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/slide-1.jpg" alt="Wanderer">
+				<div class="caption">
+					<h3>Addict coding</h3>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<!-- //Slider -->
+</body>
+</html>
